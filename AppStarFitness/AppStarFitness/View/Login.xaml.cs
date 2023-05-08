53d77cfff138563be76b4f64ef6188851938f611
gaboliveiradev <g38789138@gmail.com>
2023-05-08 15:12:30 -0300

    feat: preenchendo uma Model Aluno e chamando o metodo AutenticarAluno da DataServiceAluno

--- AppStarFitness/AppStarFitness/View/Login.xaml.cs
+++ AppStarFitness/AppStarFitness/View/Login.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AppStarFitness.DataService;
+using AppStarFitness.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,14 +20,26 @@ namespace AppStarFitness.View
             logo.Source = ImageSource.FromResource("AppStarFitness.View.logo.png");
         }
 
-        private void btn_entrar_Clicked(object sender, EventArgs e)
+        private async void btn_entrar_Clicked(object sender, EventArgs e)
         {
-            string[] cpf_pontuado = usuario.Text.Split('.', '-');
-            string cpf_digitado = cpf_pontuado[0] + cpf_pontuado[1] + cpf_pontuado[2] + cpf_pontuado[3];
-            string senha_digitada = senha.Text;
+            try
+            {
+                string[] cpf_pontuado = usuario.Text.Split('.', '-');
+                string cpf_digitado = cpf_pontuado[0] + cpf_pontuado[1] + cpf_pontuado[2] + cpf_pontuado[3];
+                string senha_digitada = senha.Text;
+
+                Aluno a = await DataServiceAluno.AutenticarAluno(new Aluno
+                {
+                    cpf = cpf_digitado,
+                    senha = senha_digitada
+                });
+            } catch (Exception err)
+            {
+                await DisplayAlert("Ops", err.Message, "OK");
+            }
 
             // CPF CADASTRADO NO BANCO
-            string cpf_cadastrado = "12345678910";
+            /*string cpf_cadastrado = "12345678910";
             string senha_cadastrada = "teste";
 
             if (cpf_digitado == cpf_cadastrado && senha_digitada == senha_cadastrada)
@@ -36,7 +50,7 @@ namespace AppStarFitness.View
             else
             {
                 DisplayAlert("Erro", "Dados incorretos!", "OK");
-            }
+            }*/
         }
 
         private void btn_esqueci_Clicked(object sender, EventArgs e)
